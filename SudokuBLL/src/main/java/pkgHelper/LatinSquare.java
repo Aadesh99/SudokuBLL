bb98078e49972f963f2d30ae24bce90931e4b55e
ANagda221 <ANagda221@users.noreply.github.com>
2018-09-12 15:41:27 -0400

    Test

---
+++ SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
@@ -0,0 +1,77 @@
+package pkgHelper;
+
+import java.util.Arrays;
+
+
+public class LatinSquare {
+	private int[][] LatinSquare;
+
+	public LatinSquare() {
+
+	}
+
+	public LatinSquare(int[][] latinSquare) {
+		super();
+		LatinSquare = latinSquare;
+	}
+
+	public boolean containsZero() {
+		boolean containsZero = false;
+
+		if (LatinSquare == null)
+			return false;
+
+		for (int iRow = 0; iRow < LatinSquare.length; iRow++) {
+			for (int iCol = 0; iCol < LatinSquare.length; iCol++) {
+				if (LatinSquare[iRow][iCol] == 0) {
+					containsZero = true;
+				}
+			}
+		}
+		return containsZero;
+
+	}
+
+	public int[][] getLatinSquare() {
+		return this.LatinSquare;
+	}
+
+	public void setLatinSquare(int[][] latinSquare) {
+		LatinSquare = latinSquare;
+	}
+
+	public boolean hasDuplicates(int[] arr) {
+		boolean hasDuplicates = false;
+
+		if (arr == null)
+			return false;
+
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] == arr[i + 1]) {
+				hasDuplicates = true;
+				break;
+			}
+		}
+
+		return hasDuplicates;
+	}
+
+	public boolean doesElementExist(int[] arr, int iValue) {
+		boolean doesElementExist = false;
+
+		if (arr == null)
+			return false;
+
+		for (int i : arr) {
+			if (i == iValue) {
+				doesElementExist = true;
+				break;
+			}
+		}
+
+		return doesElementExist;
+	}
+	
+
+}
--- SudokuBLL/src/test/java/pkgTest/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author aades
- *
- */
-package pkgTest;
